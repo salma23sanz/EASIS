fa9d320e7c761fc5f7da5ab2ae16b15248c0f3da
salma23sanz <salma.23.a.red@gmail.com>
2025-05-10 11:23:45 -0600

    Proyecto

---
+++ app/src/main/java/com/example/bar/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.bar;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Toast;
+import com.example.bar.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Window window = getWindow();
+        window.setStatusBarColor(Color.parseColor("#3FB356"));  // Aquí puedes definir el color que deseas
+        MaterialToolbar topAppBar = findViewById(R.id.topAppBar);
+
+        topAppBar.setOnMenuItemClickListener(new MaterialToolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.action_favorite) {
+                    Toast.makeText(MainActivity.this, "Favorito", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (id == R.id.action_search) {
+                    Toast.makeText(MainActivity.this, "Buscar", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (id == R.id.action_more) {
+                    Toast.makeText(MainActivity.this, "Más opciones", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                if (id == R.id.action_settings) {
+                    Toast.makeText(MainActivity.this, "Configuración", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (id == R.id.action_help) {
+                    Toast.makeText(MainActivity.this, "Ayuda", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+
+
+                return false;
+            }
+        });
+
+    }
+}
